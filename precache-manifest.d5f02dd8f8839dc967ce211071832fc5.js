aa8bdf2fa092ecf2aa69c50d0e96c140d38cb912
cpharshaw <cpharshaw+git@gmail.com>
2019-04-16 00:10:25 -0400

    Updates

---
+++ precache-manifest.d5f02dd8f8839dc967ce211071832fc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  },
+  {
+    "revision": "08e0b6afb6180692841a",
+    "url": "/clickyGame/static/js/main.82e0d0f0.chunk.js"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/clickyGame/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "08e0b6afb6180692841a",
+    "url": "/clickyGame/static/css/main.385c52bf.chunk.css"
+  },
+  {
+    "revision": "f35ba5a645ed05048821d39d37fa1ab8",
+    "url": "/clickyGame/index.html"
+  }
+];
